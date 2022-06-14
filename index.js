62e03079ebdc6498bf117489052dc466b2fb09b2
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-06-14 19:24:36 +0300

    commit tree cc9ae637c8d631f9c8239a2a81c7c0201b715cb2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const saturdayFun = function(activity1="roller-skate", activity2="bathe my dog") {
+    if (activity1){
+      return ( `This Saturday, I want to ${activity1}!`)
+    }
+    else if(activity2) {
+      return ( `This Saturday, I want to ${activity2}!`)
+    }
+}
+const mondayWork = function(activity1="go to the office", activity2="work from home") {
+    if (activity1){
+        return ( `This Monday, I will ${activity1}.`)
+      }
+      else if(activity2) {
+        return ( `This Monday, I will ${activity2}.`)
+      } 
+}
+    const wrapAdjective = function(style="*") {
+    return function (adj ="special") {
+  return `You are ${style}${adj}${style}!`
+    }
+  }  
